b67a5788533257a44fdde77b87c772de16ef1cd8
Galacthor <ifuerst2005@gmail.com>
2021-03-08 21:58:13 +0000

    added a variable cur

--- main.py
+++ main.py
@@ -51,28 +51,28 @@ async def on_ready():
 @client.event
 async def on_message(message):
 	msg = message.content
-
+	cur = '$cur'
 	add_currency(message)
 
 	if message.author == client.user:
 		return
 
-	if msg.startswith('.cur') and msg.endswith('gain'):
+	if msg.startswith(cur) and msg.endswith('gain'):
 		await message.channel.send('You gained 10 currency!')
 
-	if msg.startswith('.cur') and msg.endswith('meme'):
+	if msg.startswith(cur) and msg.endswith('meme'):
 		await message.channel.send(get_meme())
 
-	if msg.startswith('.cur') and msg.endswith('fight monster'):
+	if msg.startswith(cur) and msg.endswith('fight monster'):
 		await message.channel.send(random.choice(creatures))
 
-	if msg.startswith('.cur') and msg.endswith('shop'):
+	if msg.startswith(cur) and msg.endswith('shop'):
 		await message.channel.send(weapons)
 
-	if msg.startswith('.cur') and msg.endswith('inventory'):
+	if msg.startswith(cur) and msg.endswith('inventory'):
 		await message.channel.send(check_currency(message))
 
-	if msg.startswith('.cur') and msg.endswith('help'):
-		await message.channel.send('.cur gain: gain currency .cur meme: post a meme .cur fight monster: still working on this .cur shop: shows weapons .cur inventory: shows your currency')
+	if msg.startswith(cur) and msg.endswith('help'):
+		await message.channel.send('.cur gain: gain currency\n.cur meme: post a meme\n.cur fight monster: still working on this\n.cur shop: shows weapons\n.cur inventory: shows your currency')
 		
 client.run(os.getenv('TOKEN'))
